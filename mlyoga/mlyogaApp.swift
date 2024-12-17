26b350af4d5c172aeab0f90721802d3296d23a95
Nick Kononov <nikitakononov93@gmail.com>
2024-12-17 14:18:42 +0200

    Initial Commit

---
+++ mlyoga/mlyogaApp.swift
@@ -0,0 +1,17 @@
+//
+//  mlyogaApp.swift
+//  mlyoga
+//
+//  Created by Nick Kononov on 17.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct mlyogaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
